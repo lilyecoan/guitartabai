787c7206fd99987c7d93135173afadaf03ded533
Lily Coan <lilycoan@wireless-10-147-20-227.public.utexas.edu>
2025-11-12 14:41:58 -0600

    redo

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const tabGeneratorRoutes = require('../public/routes/tab-generator');
+const tabGeneratorRoutes = require('./routes/tab-generator');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
